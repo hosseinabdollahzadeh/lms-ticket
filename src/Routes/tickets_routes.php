e21d5f15b1123949b7d04809be614de318edd962
hossein <abdollahzadeh.hossein@gmail.com>
2023-04-23 03:11:18 +0330

    init

---
+++ src/Routes/tickets_routes.php
@@ -0,0 +1,6 @@
+<?php
+Route::group(["middleware" => "auth"], function ($router){
+    $router->resource('tickets', 'TicketController');
+    $router->post('tickets/{ticket}/reply', 'TicketController@reply')->name('tickets.reply');
+    $router->get('tickets/{ticket}/close', 'TicketController@close')->name('tickets.close');
+});
